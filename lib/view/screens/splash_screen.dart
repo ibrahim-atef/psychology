7e3c567e0d4a4e5e089ffac5931fe3cc2eecc012
IBRAHIMatef69 <86667626+IBRAHIMatef69@users.noreply.github.com>
2022-03-10 21:04:57 +0200

    splash and files

--- lib/view/screens/splash_screen.dart
+++ lib/view/screens/splash_screen.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:psychology/controller/controllers/splash_controller.dart';
+
 class SplashScreen extends StatelessWidget {
-    SplashScreen({Key? key}) : super(key: key);
-final splashController=Get.find<SplashController>();
+  SplashScreen({Key? key}) : super(key: key);
+  final splashController = Get.find<SplashController>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Splash Screen"),),);
+    return Container(
+      decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment(0.0, -1.0),
+              end: Alignment(0, 1.2),
+              colors: [
+            Color(0xff5599ff),
+            Colors.purpleAccent,
+          ])),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [Image.asset("assets/images/splash.png"),SizedBox(height: 140,)],
+        ),
+      ),
+    );
   }
 }
