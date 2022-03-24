29f7833c9e50a10cf596a62471293c757b87a27e
IBRAHIMatef69 <86667626+IBRAHIMatef69@users.noreply.github.com>
2022-03-24 12:25:39 +0200

    splash and files

--- lib/view/widgets/patient_screens_widgets/chat_widget/chat_list_tile.dart
+++ lib/view/widgets/patient_screens_widgets/chat_widget/chat_list_tile.dart
@@ -1,7 +1,6 @@
 
 import 'package:flutter/material.dart';
 import 'package:psychology/view/widgets/patient_screens_widgets/chat_widget/chat_room_list_tile.dart';
-import 'package:psychology/view/widgets/patient_screens_widgets/doctor_profile_view_for_patient_widgets/circule_image_avatar.dart';
 
 class ChatListTile extends StatelessWidget {
   const ChatListTile({Key? key}) : super(key: key);
