ded31f68820162db1e7c4b4a2e47bcf6ea97d246
Ibrahim653 <ebrahemafroto653@gmail.com>
2022-06-28 19:46:19 +0200

    testing

--- lib/view/screens/doctor_screens/appointment_details_screen.dart
+++ lib/view/screens/doctor_screens/appointment_details_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 import 'package:psychology/model/appointment_model.dart';
 import 'package:psychology/utils/constants.dart';
+import 'package:psychology/utils/styles.dart';
 import 'package:psychology/view/widgets/utils_widgets/text_utils.dart';
 
 class AppointmentDetailsScreen extends StatelessWidget {
@@ -11,6 +11,7 @@ class AppointmentDetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xfff3f4fe),
       appBar: AppBar(
         title: KTextUtils(
             text: "Details",
@@ -25,119 +26,154 @@ class AppointmentDetailsScreen extends StatelessWidget {
         child: Column(
           children: [
             CircleAvatar(
-              backgroundImage:NetworkImage("${appointmentModel.patientImage}"),
-              radius: 55,
+              backgroundImage: NetworkImage("${appointmentModel.patientImage}"),
+              radius: 70,
             ),
-            SizedBox(
-              height: 10,
+            const Spacer(
+              flex: 1,
             ),
-            SizedBox(
-              height: 60,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.grey,
+            Card(
+              color: Color(0xfff3f6ff),
+              elevation: 10,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20)),
+              child: Column(
+                children: [
+                  SizedBox(height: 10),
+                  SizedBox(
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Name : ",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(
+                          "${appointmentModel.patientName}",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: mainColor2,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  SizedBox(
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Date  : ",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(
+                          "${appointmentModel.dayDate}",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: mainColor2,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  SizedBox(
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Start : ",
+                            style: TextStyle(
+                                color: Colors.green,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)),
+                        Text(
+                          "${appointmentModel.startTime}",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: mainColor2,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  SizedBox(
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("End : ",
+                            style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold)),
+                        Text(
+                          "${appointmentModel.endTime}",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: mainColor2,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                  Divider(),
+                  SizedBox(
+                    height: 50,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text("Price : ",
+                            style: TextStyle(
+                                fontSize: 20, fontWeight: FontWeight.bold)),
+                        Text(
+                          "${appointmentModel.price}  EGP",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: mainColor2,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+            Container(
+              width: Get.width * .5,
+              decoration: BoxDecoration(
+                color: mainColor,
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: MaterialButton(
+                onPressed: () {},
                 child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("Name : ", style: TextStyle(fontSize: 20)),
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: const [
                     Text(
-                      "${appointmentModel.patientName}",
-                      style: TextStyle(fontSize: 20),
-                    )
+                      "Message",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 17),
+                    ),
+                    SizedBox(width: 10),
+                    Icon(
+                      IconBroken.Chat,
+                      size: 25,
+                      color: mainColor2,
+                    ),
                   ],
                 ),
               ),
             ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 60,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.grey,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("Date  : ", style: TextStyle(fontSize: 20)),
-                    Text( " ${appointmentModel.dayDate }"
-
-
-                        ,style: TextStyle(fontSize: 20),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 60,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.grey,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("Start : ", style: TextStyle(fontSize: 20)),
-                    Text(
-                      "${appointmentModel.startTime}",
-                      style: TextStyle(fontSize: 20),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 60,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.grey,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("End : ", style: TextStyle(fontSize: 20)),
-                    Text(
-                      "${appointmentModel.endTime}",
-                      style: TextStyle(fontSize: 20),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            SizedBox(
-              height: 60,
-              child: Card(
-                elevation: 5,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20)),
-                color: Colors.grey,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text("Price : ", style: TextStyle(fontSize: 20)),
-                    Text(
-                      "${appointmentModel.price} EGP",
-                      style: TextStyle(fontSize: 20),
-                    )
-                  ],
-                ),
-              ),
+            const Spacer(
+              flex: 4,
             ),
           ],
         ),
